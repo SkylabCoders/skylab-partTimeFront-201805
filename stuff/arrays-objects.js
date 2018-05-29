696132556976483660de49002b3129afadad65b3
Ari <arielcimax@gmail.com>
2018-05-29 14:05:13 +0200

    Arrays challenge 70%

--- stuff/arrays-objects.js
+++ stuff/arrays-objects.js
@@ -458,6 +458,53 @@ mostFreq();
 
 // ### moveElement Write a JavaScript function to move an array element from one position to another.
 
+
+var sampleArray = [];
+var copyArray = [];
+var mover = '';
+var first = 0;
+var second = 0;
+var trans = 0;
+
+
+elementos = Math.round(Math.random()*20);
+
+// GENERA EL ARRAY
+for (var i=0; i<elementos; i++){
+    sampleArray[i] = (Math.round(Math.random()*10));
+    copyArray[i] = sampleArray[i];
+};
+
+    
+mover = prompt('El array es:  '+ sampleArray + '\n\nDesea mover elementos?','si / no');
+
+if (mover.toLowerCase() === 'si' ){
+
+    function moveElements(){
+        first = parseInt(prompt('El array es:  '+ sampleArray + '\n\nIndique el elemento 1 que desea mover','primer elemento'));
+        if(first > sampleArray.length || first < 0){
+            prompt('Esa posición no existe ! \n La posición debe estar entre 0 y '+(elementos-1));
+        };
+        second = parseInt(prompt('El array es:  '+ sampleArray + '\n\nIndique el elemento final del bloque/elemento que desea mover','segundo elemento'));
+        if(second > sampleArray.length || second < 0){
+            prompt('Esa posición no existe ! \n La posición debe estar entre 0 y '+(elementos-1));
+        };
+
+        trans = sampleArray[first];
+        sampleArray[first] = sampleArray[second];
+        sampleArray[second] = trans;
+
+        mover = prompt('El array original es:   ' + copyArray + '\n\nElemento 1 = ' + first + '\nElemento 2 = ' + end + '\n\nEl array modificado es:   ' + sampleArray + '\n\nDesea hacer más cambios?', 'si / no');
+
+
+        if (mover.toLowerCase() === 'si' ){
+            moveElements();
+        }
+
+    } 
+} 
+
+moveElements();
 /*
     Test Data :
     console.log(move([10, 20, 30, 40, 50], 0, 2));
@@ -468,11 +515,12 @@ mostFreq();
 
 
 
+
+//### swapCase: Write a JavaScript program which accept a string as input and swap the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.
+
+
+
 /*
-### swapCase
-
-Write a JavaScript program which accept a string as input and swap the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.
-
 ### Print elements array
 
 Write a JavaScript program which prints the elements of the following array.
