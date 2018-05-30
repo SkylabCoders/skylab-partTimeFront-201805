a1c34cd2cd1bee12b3d26c52a07bdba7b7838f90
Leticia Morlans <letgomorlans@gmail.com>
2018-05-30 21:38:32 +0200

    add var types

---
+++ staff/Leticia/js/var-types.js
@@ -0,0 +1,65 @@
+// # Data types and operators exercices 
+
+// ## What type you think they are?
+
+// ```js
+typeof(15)
+// Prediction: Number
+// Actual:
+
+typeof(5.5)
+// Prediction:Number
+// Actual:
+
+typeof(NaN)
+// Prediction: Number ("Not a number" es un tipo de número, resultado imposible de calcular, como infinity etc..)
+// Actual:
+
+typeof("hello")
+// Prediction: string
+// Actual:
+
+typeof(true)
+// Prediction: boolean
+// Actual:
+
+typeof(1 != 2)
+// Prediction: boolean
+// Actual:
+
+
+"hamburger" + "s"
+// Prediction: hamburgers
+// Actual:
+
+"hamburgers" - "s"
+// Prediction: NaN
+// Actual:
+
+"1" + "3"
+// Prediction: 13
+// Actual:
+
+"1" - "3"
+// Prediction: -2 (nos lo transforma en numeros y calcula)
+// Actual:
+
+"johnny" + 5
+// Prediction: johnny5 
+// Actual:
+
+"johnny" - 5 
+// Prediction: NaN
+// Actual:
+
+"johnny" + "5"
+// Prediction: string
+// Actual:
+
+"johnny" - "5" 
+// Prediction: NaN
+// Actual:
+
+99 * "luftbaloons"
+// Prediction: NaN
+// Actual:
