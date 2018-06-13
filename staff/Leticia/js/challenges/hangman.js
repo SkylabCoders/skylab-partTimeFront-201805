c7a47c7306c0e76f2a6f6517055ee50fcebee3c0
Leticia Morlans <letgomorlans@gmail.com>
2018-06-13 22:26:13 +0200

    added hangman and modiied slick

--- staff/Leticia/js/challenges/hangman.js
+++ staff/Leticia/js/challenges/hangman.js
@@ -9,18 +9,16 @@
 // Si se agotan las vidas del usuario, mostrar un mensaje "GAME OVER"
 // Si se adivina la palabra, mostrar un mensaje "YOU WIN"
 
-var greekGods = ['Achelous','Aeolus','Aether','Alastor','APOLLO','ARES','Aristaeus','Asclepius','ATLAS','Attis','Boreas','Caerus','Castor','Cerus','CHAOS','Charon','Cronos','Crios','CRONUS','Dinlas','DIONYSUS'];
+
+var greekGods = ['Achelous']//,'Aeolus','Aether','Alastor','APOLLO','ARES','Aristaeus','Asclepius','ATLAS','Attis','Boreas','Caerus','Castor','Cerus','CHAOS','Charon','Cronos','Crios','CRONUS','Dinlas','DIONYSUS'];
 
 var lifes = 8;
-var wordToGuess = "";
 var userProgress = [];
 
 //function chose a word 
 function chooseWord(arr){
     randomPos = Math.floor(Math.random() * arr.length);
-    wordToGuess = greekGods[randomPos].split('');
-    
-    return wordToGuess;
+    return greekGods[randomPos].split('');
 }
 
 function encriptWord(arr) {
@@ -32,34 +30,115 @@ function encriptWord(arr) {
     return wordEncripted; 
 }
 
-function playLetter(wordToGuess, wordEncripted){
-    var letter = "";
-
-    var letter = window.prompt('¿Qué letra quieres jugar?'); // [3] ask for letter
-        
+function playLetter(wordToGuess, status){
+    var letter = window.prompt('¿Qué letra quieres jugar? ESTADO: '+status.word.join('')); // [3] ask for letter    
+    var esta = false;
+  
     //comprobacion de letra dentro de array
-    for(var i = 0; i < word.length; i++) {
+    for(var i = 0; i < wordToGuess.length; i++) {
         if(letter === wordToGuess[i]){
-            wordEncripted[i] = wordToGuess[i];
+            status.word[i] = wordToGuess[i];
+            esta = true;
         }
     }
-    return wordEncripted;
-
-    console.log(wordEncripted.join(''));
+    
+    if(!esta){
+      status.lifes--;  
+    }
+    
+console.log(status.word, wordToGuess);
+    if(status.word.join('') === wordToGuess.join('')){
+       status.current = "success";
+        
+    }else if(status.lifes == 0){
+      status.current = "fail";
+    }
+    
+    return status;
+  
 }
 
 
 function hangman(arr){
-    
-    chooseWord(arr); // [1] choose a word 
-
-    encriptWord(wordToGuess); // [2] encript that word
-    
-    if(wordEncripted !== wordToGuess) {
-        playLetter(wordToGuess, wordEncripted); // [3] ask for letter
-    } else {
-        console.log('Congratulations!')
+    var wordToGuess = chooseWord(arr); // [1] choose a word 
+  
+    var status = {
+      lifes: 8,
+      word : encriptWord(wordToGuess),
+      current: "running"
+    }
+  
+    while (status.current === "running"){
+      status = playLetter(wordToGuess, status);
+      // {lifes: 7, word:['_'], current: 'running' }
+      console.log(status);
+      
+    }
+  
+    if(status.current === "success"){
+      alert("YOU WIN");
+      
+    }else if(status.current === "fail"){
+      alert("GAME OVER");
     }
 }
 
 hangman(greekGods);
+
+
+
+
+// var greekGods = ['Achelous','Aeolus','Aether','Alastor','APOLLO','ARES','Aristaeus','Asclepius','ATLAS','Attis','Boreas','Caerus','Castor','Cerus','CHAOS','Charon','Cronos','Crios','CRONUS','Dinlas','DIONYSUS'];
+
+// var lifes = 8;
+// var wordToGuess = "";
+// var userProgress = [];
+
+// //function chose a word 
+// function chooseWord(arr){
+//     randomPos = Math.floor(Math.random() * arr.length);
+//     wordToGuess = greekGods[randomPos].split('');
+    
+//     return wordToGuess;
+// }
+
+// function encriptWord(arr) {
+//     var wordEncripted = []; 
+    
+//     for(i=0;i<arr.length;i++){
+//         wordEncripted.push(' _ ');
+//     }
+//     return wordEncripted; 
+// }
+
+// function playLetter(wordToGuess, wordEncripted){
+//     var letter = "";
+
+//     var letter = window.prompt('¿Qué letra quieres jugar?'); // [3] ask for letter
+        
+//     //comprobacion de letra dentro de array
+//     for(var i = 0; i < word.length; i++) {
+//         if(letter === wordToGuess[i]){
+//             wordEncripted[i] = wordToGuess[i];
+//         }
+//     }
+//     return wordEncripted;
+
+//     console.log(wordEncripted.join(''));
+// }
+
+
+// function hangman(arr){
+    
+//     chooseWord(arr); // [1] choose a word 
+
+//     encriptWord(wordToGuess); // [2] encript that word
+    
+//     if(wordEncripted !== wordToGuess) {
+//         playLetter(wordToGuess, wordEncripted); // [3] ask for letter
+//     } else {
+//         console.log('Congratulations!')
+//     }
+// }
+
+// hangman(greekGods);
