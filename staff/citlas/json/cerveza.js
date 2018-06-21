7463309fd6da3d7b2a5e41e91d16375f96eee857
citlas <citlalli.prado@gmail.com>
2018-06-21 16:28:07 +0200

    muestra detalle en la misma pagina, no en la nueva

--- staff/citlas/json/cerveza.js
+++ staff/citlas/json/cerveza.js
@@ -1,4 +1,4 @@
-
+   
  console.log("INI");
  $(document).ready(function(){
    console.log("DOCUMENT READY INI");
@@ -12,8 +12,8 @@
        success: function(data){
          data.forEach(function(beer){
            $('.container').append(
-             '<div class="beer">'+
-               '<div class="beer-image" data-name='+beer.name+' style="background-image: url('+beer.image_url+')"></div>'+
+             '<div class="beer" data-id='+beer.id+'>'+
+               '<div class="beer-image"  style="background-image: url('+beer.image_url+')"></div>'+
                '<div class="beer-info">'+
                  '<h1>'+beer.name+'</h1>'+
                  '<h2>'+beer.tagline+'</h2>'+
@@ -103,15 +103,51 @@ function searchBeer(searchThis){
 
 $(document).on('click', '.beer', function(){//https://stackoverflow.com/questions/9484295/jquery-click-not-working-for-dynamically-created-items
  console.log("The beer was clicked.");
- //var myWindow = window.open("cervezasDetalle.html", "MsgWindow","");
+ var myWindow = window.open("cervezasDetalle.html");
+ //$("#containerDetail").html("your text here");
+ //$(myWindow.document.body).html("<h2>Hello World!</h2>");
+ //myWindow.document.write('<h2>Hello World!</h2>');
  //myWindow.document.write("<h2>Hello World!</h2>")
- output = '<h1>Hello, World!</h1>';
- window.open('cervezasDetalle.html').document.body.innerHTML += output;
+ //var productDet = $(this).attr("data-id").text()
+ //console.log($(this))
+ //console.log( $(this).data('id'))
+
+
+/* No funciona
+//https://stackoverflow.com/questions/10472927/add-content-to-a-new-open-window
+
+in parent.html:
+<script type="text/javascript">
+    $(document).ready(function () {
+        var output = "data";
+        var OpenWindow = window.open("child.html", "mywin", '');
+        OpenWindow.dataFromParent = output; // dataFromParent is a variable in child.html
+        OpenWindow.init();
+    });
+
+in child.html:
+<script type="text/javascript">
+    var dataFromParent;    
+    function init() {
+        document.write(dataFromParent);
+    }
+*/
+var idToShow = $(this).attr('data-id');
+console.log(idToShow);
  
- $.ajax({
-       url: "https://api.punkapi.com/v2/beers?beer_name="+$(this).attr("data-name"),
-       success: function(beer){
-           $('.containerDetail').append(
+        myWindow.dataFromParent = idToShow; // dataFromParent is a variable in child.html
+       // myWindow.init();
+
+
+ //ya funciona presentar la info, solo falta que la muestre en la nueva html
+ 
+
+function details(){
+  $.ajax({
+       url: "https://api.punkapi.com/v2/beers/"+idToShow,
+       success: function(data){
+         data.forEach(function(beer){
+           $('.container').append(
              '<div class="beer">'+
                '<div class="beer-image" style="background-image: url('+beer.image_url+')"></div>'+
                '<div class="beer-info">'+
@@ -125,10 +161,9 @@ $(document).on('click', '.beer', function(){//https://stackoverflow.com/question
                  '<h2> Ingredients: '+beer.ingredients+'</h2>'+
                  '<h2> Food pairing: '+beer.food_pairing+'</h2>'+
                  '<h2> Brewers tips: '+beer.brewers_tips+'</h2>'+
-                 '<h2>'+beer.tagline+'</h2>'+
                '</div>'+
              '</div>');
-        
+        });
        },//termina success
        error: function(request, status, message){
          console.log("ERROR");
@@ -138,7 +173,8 @@ $(document).on('click', '.beer', function(){//https://stackoverflow.com/question
        }//termina error
        
      });//Termina ajax
-
+}
+details();
 });//termina document on click
 
 
